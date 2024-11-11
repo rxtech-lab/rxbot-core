08b7fe1b696a9b86a4cb845432238f7c59ccb0a2
sirily11 <32106111+sirily11@users.noreply.github.com>
2024-11-11 13:27:39 +0800

    fix: add test for link (#183)

---
+++ tests/telegram-e2e-tests/src/text/link/link.spec.tsx
@@ -0,0 +1,51 @@
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import { Api, MessageType } from "@rx-lab/mock-telegram-client";
+import {
+  DEFAULT_RENDERING_WAIT_TIME,
+  PORT,
+  initialize,
+  sleep,
+} from "../../utils";
+
+const chatroomId = 1200;
+
+describe("Render link", () => {
+  let api: Api<any>;
+  let coreApi: any;
+
+  beforeAll(async () => {
+    api = new Api({
+      baseUrl: `http://0.0.0.0:${PORT}`,
+    });
+  });
+
+  it("should render link properly", async () => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const rootDir = path.join(__dirname, "src");
+    const destinationDir = path.join(__dirname);
+    const { core } = await initialize(chatroomId, api, {
+      rootDir,
+      destinationDir,
+    });
+    coreApi = core;
+
+    await api.chatroom.sendMessageToChatroom(chatroomId, {
+      content: "/start",
+      type: MessageType.Text,
+    });
+    await sleep(DEFAULT_RENDERING_WAIT_TIME);
+    let messages = await api.chatroom.getMessagesByChatroom(chatroomId);
+    expect(messages.data.count).toBe(2);
+    let currentMessage = messages.data.messages[1];
+    expect(currentMessage?.update_count).toBe(0);
+    expect(currentMessage?.text).toBe(
+      'Hello world with <a href="https://example.com">link</a>',
+    );
+  });
+
+  afterEach(async () => {
+    await coreApi.onDestroy();
+  });
+});
---
+++ tests/telegram-e2e-tests/src/text/link/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      Hello world with <a href="https://example.com">link</a>
+    </div>
+  );
+}
