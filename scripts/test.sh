7a3c4c0eeee81ca4f08952da3c7c156a6b4b8572
sirily11 <32106111+sirily11@users.noreply.github.com>
2025-01-16 16:32:59 +0800

    fix: add typing indication to adapter (#310)

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,8 @@
+# Define version variable
+MOCK_TELEGRAM_VERSION="1.3.8"
+
 # Download the mock-telegram server
-wget https://github.com/rxtech-lab/mock-telegram-server/releases/download/v1.3.7/mock-telegram-linux-x86_64 -O mock-telegram
+wget "https://github.com/rxtech-lab/mock-telegram-server/releases/download/v${MOCK_TELEGRAM_VERSION}/mock-telegram-linux-x86_64" -O mock-telegram
 chmod +x mock-telegram
 
 # Add the mock-telegram server to the PATH
